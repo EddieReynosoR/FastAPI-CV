57250a8664e1407ef1f420bc23ae215663d321d6
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2026-03-19 10:15:20 -0700

    agregar load_env

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from api.test import router as gemini_router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+app = FastAPI()
+
+app.include_router(gemini_router, prefix="/api")
